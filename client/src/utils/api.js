2bce0c7eaae3dd6078f4f9dc62e7872ee7513ae8
js-hari <96908360+js-hari@users.noreply.github.com>
2025-02-16 15:36:52 +0530

    Update api.js

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "http://localhost:5000";
+const API_BASE_URL = process.env.NEXT_PUBLIC_API_BASE_URL || "https://log-parser-backend-68m4.onrender.com";
 
 // Generic API request function
 const fetchData = async (endpoint) => {

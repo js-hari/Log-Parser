58ffe2583b58c892cfc8e553aa7fdf7c860ef476
js-hari <96908360+js-hari@users.noreply.github.com>
2025-02-18 14:48:20 +0530

    Update server.js

--- server/src/server.js
+++ server/src/server.js
@@ -6,13 +6,23 @@ const trafficRoutes = require("./routes/trafficRoutes");
 
 const app = express();
 
-app.use(
-  cors({
-    origin: "https://log-parser-mgvo.onrender.com", 
-    methods: "GET,POST,PUT,DELETE",
-    allowedHeaders: "Content-Type,Authorization",
-  })
-);
+// app.use(
+//   cors({
+//     origin: "https://log-parser-mgvo.onrender.com", 
+//     methods: "GET,POST,PUT,DELETE",
+//     allowedHeaders: "Content-Type,Authorization",
+//   })
+// );
+
+app.use(cors()); 
+
+
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "https://log-parser-mgvo.onrender.com");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
 app.use(express.json());
 
